59751d48e860e62e5b520a2f3c99092506147455
unknown <dnicerio@gmail.com>
2020-07-07 01:47:24 +0800

    create Blog Schema & Model then integrate into MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // Imports
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // Express App
 const app = express();
 
+// Connect to MongoDB
+const dbURI = 'mongodb+srv://dnicerio:HOTCAKE123@blog-project.9ey8o.mongodb.net/blog-project?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // Register View Engine
 app.set('view engine', 'ejs');
-
-// Listen for HTTP requests
-app.listen(3000);
 
 // Express static files
 app.use(express.static('public'));
@@ -17,19 +22,25 @@ app.use(express.static('public'));
 // Morgan HTTP Request Logger
 app.use(morgan('dev'));
 
-// Send matching HTML file on HTTP response
+// Routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'How to win in life', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
-    {title: 'When to get first car', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
-    {title: 'All your base are belong to us', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 })
+
+// Blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'Home', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new Blog' });
