a1ccd0c88abb81c16f44c48fbc5a1b8c4bf6d278
unknown <dnicerio@gmail.com>
2020-07-08 00:56:37 +0800

    add GET, POST and DELETE functionality to app

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // Register View Engine
 app.set('view engine', 'ejs');
 
-// Express static files
+// Express middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Morgan HTTP Request Logger
 app.use(morgan('dev'));
@@ -44,6 +45,45 @@ app.get('/blogs', (req, res) => {
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new Blog' });
+})
+
+// Handle POST Blog data
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Find Blog by Id and render Blog details
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Delete Blog data
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 
 // 404 Page
